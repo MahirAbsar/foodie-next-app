68a828202f3342a43b4a77e68339a3a02a9be4ce
Mahir <mahirabsar181@gmail.com>
2024-05-06 01:51:42 +0600

    feat: setup next app for foodie app

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,5 @@
+const SingleMealPage = ({ params }) => {
+  console.log(params);
+  return <h1>Single Meal Page</h1>;
+};
+export default SingleMealPage;
---
+++ components/main-header.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import logoImg from "@/assets/logo.png";
+
+import classes from "./main-header.module.css";
+
+export default function MainHeader() {
+  return (
+    <header className={classes.header}>
+      <Link href="/" className={classes.logo}>
+        <Image src={logoImg} alt="the food displayed on the plate" priority />
+        NextLevel Food
+      </Link>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <Link href="/meals">Browse Meals</Link>
+          </li>
+          <li>
+            <Link href="/community">Foodie Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
