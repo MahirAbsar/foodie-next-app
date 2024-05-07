5bf53cecf091e05bc7641de32d6acfcf4434ed57
Mahir <mahirabsar181@gmail.com>
2024-05-07 20:48:49 +0600

    feat: make the navlinks active dynamically as a client component

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -5,6 +5,7 @@ import logoImg from "@/assets/logo.png";
 
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "../nav-link/nav-link";
 
 export default function MainHeader() {
   return (
@@ -18,10 +19,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodie Community</Link>
+              <NavLink href="/community">Foodie Community</NavLink>
             </li>
           </ul>
         </nav>
