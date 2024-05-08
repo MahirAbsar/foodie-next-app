ef095258cb1c80b042d8a452a8ce2757171895b1
Mahir <mahirabsar181@gmail.com>
2024-05-08 14:32:11 +0600

    feat: implement not-found and error pages for meals

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <main className="not-found">
+      <h1>NOT FOUND</h1>
+      <p>Sorry, could not find the requested page or resources.</p>
+    </main>
+  );
+}
