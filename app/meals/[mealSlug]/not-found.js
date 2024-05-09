960516d6261bc67192c691c8e87306c2044a4130
Mahir <mahirabsar181@gmail.com>
2024-05-09 09:02:36 +0600

    feat: complete single meals page

---
+++ app/meals/[mealSlug]/not-found.js
@@ -0,0 +1,9 @@
+export default function NotFoundPage() {
+    return (
+      <main className="not-found">
+        <h1>MEAL NOT FOUND</h1>
+        <p>Sorry, could not find the requested meal data.</p>
+      </main>
+    );
+  }
+  
